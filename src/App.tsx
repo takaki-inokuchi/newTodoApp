b30da4420c090d8764d2ff9cf4a8a88305e6e4d4
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-19 18:49:03 +0900

    yaml

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Provider } from "./components/ui/provider";
+import { Button } from "@chakra-ui/react";
+
 function App() {
   return (
-    <>
+    <Provider>
       <h1>Hello World!</h1>
-    </>
+      <Button>fff</Button>
+    </Provider>
   );
 }
 
