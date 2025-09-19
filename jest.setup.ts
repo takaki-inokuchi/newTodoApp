92a79ef97c46bcb6f00ffaf8f7f936d20c628ce8
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-09-19 18:59:19 +0900

    yaml

--- jest.setup.ts
+++ jest.setup.ts
@@ -10,3 +10,16 @@ if (typeof structuredClone === "undefined") {
   // Node.js で structuredClone が未定義の場合にポリフィル
   global.structuredClone = <T>(obj: T) => JSON.parse(JSON.stringify(obj));
 }
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),       // 古いブラウザ互換
+    removeListener: jest.fn(),    // 古いブラウザ互換
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  }),
+});
